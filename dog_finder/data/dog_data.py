5e187566b7a527942facb47a61ad5777bc856834
jojo-en-code <joel.enayat@code.berlin>
2021-05-05 09:34:58 +0200

    test

--- dog_finder/data/dog_data.py
+++ dog_finder/data/dog_data.py
@@ -1,13 +1,13 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+# scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+# creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
-client = gspread.authorize(creds)
+# client = gspread.authorize(creds)
 
-sheet = client.open("dog_db").sheet1
+# sheet = client.open("dog_db").sheet1
 
 # if os.path.isfile('filepath'):
 #     credentials = ServiceAccountCredentials.from_json_keyfile_name('filepath',
